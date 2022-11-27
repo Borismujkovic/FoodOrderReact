93dc003839240180200f0c4453b59a11aa85dd59
unknown <borismujkovic@gmail.com>
2022-11-27 17:44:44 +0100

    final comit

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -31,7 +31,7 @@ export const HomePage = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://foodorder-e6ac4-default-rtdb.firebaseio.com/meals.json"
+        "https://orderfoodapp-a25f5-default-rtdb.firebaseio.com/meals.json"
       );
 
       if (!response.ok) {
@@ -71,7 +71,7 @@ export const HomePage = () => {
 
   const submitOrderHandler = async (userData) => {
     await fetch(
-      "https://foodorder-e6ac4-default-rtdb.firebaseio.com/orders.json",
+      "https://orderfoodapp-a25f5-default-rtdb.firebaseio.com/orders.json",
       {
         method: "POST",
         body: JSON.stringify({
